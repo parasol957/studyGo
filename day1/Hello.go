7710f281ef20d06cda8fe0aeb0e9f39b982d75ce
parasol <619372857@qq.com>
2020-05-21 14:54:33 +0800

    转义字符的学习

--- day1/Hello.go
+++ day1/Hello.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("你好\t世界") //测试一下
+	fmt.Println("你好\t世界") //一个制表位，实现对齐的功能
+	fmt.Println("你好\n世界") //实现换行的功能
+	fmt.Println("你好\\世界") //一个\
+	fmt.Println("你好\"世界") //一个"
+	fmt.Println("你好\r世界") //实现换行的功能
 
 }
