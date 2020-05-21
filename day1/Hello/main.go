7b44e219918b962bce0b94df5e940c9aeaf97dff
parasol <619372857@qq.com>
2020-05-21 15:08:41 +0800

    修改/r

--- day1/Hello/main.go
+++ day1/Hello/main.go
@@ -7,6 +7,6 @@ func main() {
 	fmt.Println("你好\n世界") //实现换行的功能
 	fmt.Println("你好\\世界") //一个\
 	fmt.Println("你好\"世界") //一个"
-	fmt.Println("你好\r世界") //实现换行的功能
+	fmt.Println("你好\r世界") //表示把'世界'顶到最前面去，覆盖掉以前的内容
 
 }
